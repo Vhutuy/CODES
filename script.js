2ab2da20e704a9be44ac290e847dac853c0c9bc2
Vhutuy <130938285+Vhutuy@users.noreply.github.com>
2023-04-17 21:36:47 -0300

    Added new files

---
+++ script.js
@@ -0,0 +1,13 @@
+const aparece = document.querySelector("#carregar")
+aparece.setAttribute("style", "display: none;");
+
+
+
+
+function cara() {
+  const aparece = document.querySelector("#carregar")
+  aparece.removeAttribute("display");
+  aparece.setAttribute("style", "display: flex;");
+
+  
+}
